65fbc5cb8d94a70f2f107ec5cff1f84cf98fba0e
ifthekar <sajalcse2018@gmail.com>
2023-05-10 17:05:15 +0600

    simple for loop

---
+++ foorLoop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int i,n;
+    printf("How Much Time You Want To Print::");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+            printf("%d.Hello For Loop\n",i);
+
+    }
+
+
+return 0;
+}
+

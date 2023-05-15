817d925d1c018b38e76ca8034ab9fab6a82897ad
ifthekar <sajalcse2018@gmail.com>
2023-05-15 15:30:28 +0600

    while loop with user input

---
+++ while.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+        int count_value,i=1;
+        printf("Enter Your Number How Much You Want To Display::");
+        scanf("%d",&count_value);
+        while(i<=count_value){
+            printf("Count::%d\n",i);
+            i++;
+        }
+return 0;
+}

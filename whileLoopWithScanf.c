dbd1d2325303270ff674faf03658f7ba85320aea
ifthekar <sajalcse2018@gmail.com>
2023-05-15 15:52:45 +0600

    user input scanf function with while loop

---
+++ whileLoopWithScanf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+   int userInputValue,i=1;
+
+   printf("Enter User Input Value=");
+   scanf("%d",&userInputValue);
+   while(i<=userInputValue){
+    printf("counting Value is::%d\n",i);
+    i++;
+   }
+
+
+return 0;
+
+}

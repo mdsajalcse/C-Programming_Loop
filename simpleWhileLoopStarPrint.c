3b7ceaa5d9e0766dff5cd77d1c9fda682b9d3349
ifthekar <sajalcse2018@gmail.com>
2023-05-15 17:59:24 +0600

    star print with while loop

---
+++ simpleWhileLoopStarPrint.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+   int rows,x=0;
+   printf("Enter Number Of Rows::");
+   scanf("%d",&rows);
+   while(x<rows){
+    printf("*\n");
+    x++;
+   }
+
+
+return 0;
+}

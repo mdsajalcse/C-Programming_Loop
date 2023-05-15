55f45f1c3d706c279914156ab0af6ccdbc94b95d
ifthekar <sajalcse2018@gmail.com>
2023-05-15 17:42:13 +0600

    while loop with logical operator

---
+++ whileLoopUsingLogicalOperator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+        int i=1,j=1,k=1;
+        while(i<=4 || j<=5 ||k<=6){
+            printf("%d %d %d\n",i,j,k);
+            i++;
+            j++;
+            k++;
+        }
+return 0;
+}
